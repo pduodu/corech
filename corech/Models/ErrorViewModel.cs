aba164e9f3f361ccd01da89c90cf6775f4a914d4
PrinceDoe <pduodu60@gmail.com>
2023-01-17 12:52:57 +0000

    Add project files.

---
+++ corech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace corech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
